7d4a8cd78e68c2084ef86146a18184763727b11a
Erika Brownlee <eebrownl@gmail.com>
2022-11-27 17:39:01 -0700

    added isFavorite and cartItems states

--- src/Context.js
+++ src/Context.js
@@ -4,19 +4,50 @@ const Context = React.createContext()
 
 function ContextProvider(props) {
     const [allPhotos, setAllPhotos] = useState([])
+    const [cartItems, setCartItems] = useState([])
     const url = 'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
 
+        console.log(cartItems)
+   
     useEffect(() => {
         fetch(url)
         .then((response) => response.json())
         .then((data) => setAllPhotos(data))
     }, [])
 
+    function toggleFavorite(id) {
+        const updatedArray = allPhotos.map(photo => {
+            if(photo.id === id) {
+                return {
+                    ...photo, 
+                    isFavorite: !photo.isFavorite
+                }
+            }
+            return photo
+        })
+        setAllPhotos(updatedArray)
+    }
+
+    function addToCart(newItem) {
+        setCartItems(prevCartItems => [...prevCartItems, newItem])
+        }
+
+    function removeFromCart(id) {
+        setCartItems(prevItems => prevItems.filter(item => item.id !== id))
+        }
+
     return(
-        <Context.Provider value={{allPhotos}}>
+        <Context.Provider value={{allPhotos, toggleFavorite, addToCart, cartItems, removeFromCart}}>
             {props.children}
         </Context.Provider>
     )
 }
 
 export {ContextProvider, Context}
+
+
+// Setup context to manage items in an array called `cartItems`. This will be an array of image objects.
+
+// 1. Add the `cartItems` state to context. (Array)
+// 2. Add function to add an image to the cart. (Takes the full image object as parameter)
+// 3. Make it so clicking the plus icon on the image adds the item to the cart. (Console.log the cart items array to see that it's working)
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,13 +1,37 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import heart from './heart-line.png'
+import add from './add-circle-line.png'
+import {Context} from '../Context'
+import PropTypes from 'prop-types'
+import cart from './shopping-cart-line.png'
 
 function Image({className, img}) {
     const [isHovered, setIsHovered] = useState(false)
+    const {toggleFavorite} = useContext(Context)
+    const {addToCart, cartItems, removeFromCart} = useContext(Context)
+
+    const heartIcon = img.isFavorite ? <img alt='heart' src={heart} className='ri-heart-fill favorite' onClick={() => toggleFavorite(img.id)}/> : 
+    isHovered && <img alt='heart' src={heart} className='ri-heart-line favorite' onClick={() => toggleFavorite(img.id)}/>
+    
+    function cartIcon() {
+        if(cartItems.find(obj => obj.id === img.id)) {
+            return <img alt='cart' src={cart} className='ri-shopping-cart-fill cart' onClick={() => removeFromCart(img.id)} />
+        }else if(isHovered) return <img alt='add' src={add} className='ri-add-circle-line cart' onClick={() => addToCart(img)}/>
+    }
     
     return(
         <div className={`${className} image-container`} onMouseEnter={()=> setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
             <img alt='pic' src={img.url} className='image-grid' />
+            {heartIcon}
+            {cartIcon()}
+            
         </div>
     )
+}
+
+Image.propTypes = {
+    classname: PropTypes.string, 
+    img: PropTypes.object
 }
 
 export default Image
