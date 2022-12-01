81b3a3996fa07e1137b9c7ee332e2f264a885d6b
Erika Brownlee <eebrownl@gmail.com>
2022-11-30 17:40:38 -0700

    added delete button logic and total calculation

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import bin from './delete-bin-line.png'
+import {Context} from '../Context'
 
 function CartItem({item}) {
+    const {removeFromCart} = useContext(Context)
     return(
         <div className='cart-item'>
-            <img src = {bin} alt='bin' className = 'ri-delete-bin-line'/>
+            <img src = {bin} alt='bin' className = 'ri-delete-bin-line' onClick={() => removeFromCart(item.id)}/>
             <img src = {item.url} alt='cart-pic' width = '130px' className='cart-pic'/>
             <p>5.99</p>
 
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,19 +1,35 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {Context} from '../Context'
 import CartItem from '../components/CartItem'
 
 function Cart() {
+    const [buttonText, setButtonText] = useState('Place Order')
     const {cartItems} = useContext(Context)
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item} />
     ))
+
+    function getTotal() {
+        const total = (cartItems.length * 5.99).toLocaleString("en-US", {style: "currency", currency: "USD"})
+        return total
+    }
+
+
+    function handleClick() {
+        setButtonText('Ordering...')
+        setTimeout(() => {
+            console.log('order placed!')
+            setButtonText('Place Order')
+        }, 3000)
+    }
+    
     return(
         <main className='cart-page'>
             <h1>Check out</h1>
             <div>{cartItemElements}</div>
-            <p className='total-cost'>Total: </p>
+            <p className='total-cost'>{`Total: ${getTotal()}`}</p>
             <div className='order-button'>
-                <button>Place Order</button>
+                <button onClick={handleClick()}>{buttonText}</button>
             </div>
         </main>
     )
